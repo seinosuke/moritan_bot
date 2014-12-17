7faafcda2ebc5bb7155122e01f91cefe0a2a0e4e
seinosuke <seinosuke.3606@gmail.com>
2014-12-18 08:50:05 +0900

    クラスメソッドをまとめた

--- lib/moritan/database/user.rb
+++ lib/moritan/database/user.rb
@@ -12,60 +12,63 @@ module Moritan
     I18n.enforce_available_locales = false
     has_one :credit, :dependent => :destroy
 
-    # 新規登録
-    def User::entry(twitter_id="")
-      user = self.new do |u|
-        u.twitter_id = twitter_id
-        u.context = ""
-        u.last_date = Time.now
-      end
-      user.save
+    class << self
 
-      # first_or_createをしなくてよくなる
-      credit = Moritan::Credit.new do |c|
-        c.user_id = user.id
-        c.aa_times = 0
-        c.a_times  = 0
-        c.b_times  = 0
-        c.c_times  = 0
-        c.d_times  = 0
-        c.gpa      = 0.00
-        c.total    = 0
-      end
-      credit.save
-    end
-
-    # 情報の削除（id指定無しで全員）
-    def User::clear(id:nil)
-      if id
-        user = self.find(id)
-        user.destroy
-        user.save
-      else
-        self.destroy_all
-      end
-    rescue
-      error_logs("clear", $!, $@)
-    end
-
-    # userの情報を表示（id指定無しで全員表示）
-    def User::show_contents(id:nil)
-      if id
-        user = self.find(id)
-        puts "id         #{user.id}"
-        puts "twitter_id #{user.twitter_id}"
-        puts "context    #{user.context}"
-        puts "last_date  #{user.last_date}"
-      else
-        self.all.each do |u|
-          puts "id         #{u.id}"
-          puts "twitter_id #{u.twitter_id}"
-          puts "context    #{u.context}"
-          puts "last_date  #{u.last_date}"
+      # 新規登録
+      def entry(twitter_id="")
+        user = self.new do |u|
+          u.twitter_id = twitter_id
+          u.context = ""
+          u.last_date = Time.now
         end
+        user.save
+
+        # first_or_createをしなくてよくなる
+        credit = Moritan::Credit.new do |c|
+          c.user_id = user.id
+          c.aa_times = 0
+          c.a_times  = 0
+          c.b_times  = 0
+          c.c_times  = 0
+          c.d_times  = 0
+          c.gpa      = 0.00
+          c.total    = 0
+        end
+        credit.save
       end
-    rescue
-      error_logs("show_contents", $!, $@)
+
+      # 情報の削除（id指定無しで全員）
+      def clear(id:nil)
+        if id
+          user = self.find(id)
+          user.destroy
+          user.save
+        else
+          self.destroy_all
+        end
+      rescue
+        error_logs("clear", $!, $@)
+      end
+
+      # userの情報を表示（id指定無しで全員表示）
+      def show_contents(id:nil)
+        if id
+          user = self.find(id)
+          puts "id         #{user.id}"
+          puts "twitter_id #{user.twitter_id}"
+          puts "context    #{user.context}"
+          puts "last_date  #{user.last_date}"
+        else
+          self.all.each do |u|
+            puts "id         #{u.id}"
+            puts "twitter_id #{u.twitter_id}"
+            puts "context    #{u.context}"
+            puts "last_date  #{u.last_date}"
+          end
+        end
+      rescue
+        error_logs("show_contents", $!, $@)
+      end
     end
   end
 end
