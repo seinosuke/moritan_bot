3e0840bc74fb14c6d984dfa8ba73f5a360ce1619
seinosuke <seinosuke.3606@gmail.com>
2015-02-08 00:30:38 +0900

    chain_postを廃止してpostを再帰的に書き換え

--- lib/moritan/bot.rb
+++ lib/moritan/bot.rb
@@ -9,16 +9,7 @@ module Moritan
       :client, :timeline
 
     def initialize(debug:false, mention:false)
-      @config = YAML.load_file(Moritan::CONF_FILE)
-
-      @function = Moritan::Function.new(@config['ReplayTable'], @config['Function'])
-      @name = debug ? @config['name_debug'] : @config['name']
-
-      oauth = debug ? 'oauth_debug' : 'oauth'
-      @CONSUMER_KEY       = @config[oauth]['consumer_key']
-      @CONSUMER_SECRET    = @config[oauth]['consumer_secret']
-      @OAUTH_TOEKN        = @config[oauth]['oauth_token']
-      @OAUTH_TOEKN_SECRET = @config[oauth]['oauth_token_secret']
+      load_config(debug)
 
       @client = Twitter::REST::Client.new do |c|
         c.consumer_key        = @CONSUMER_KEY
@@ -39,60 +30,43 @@ module Moritan
       end
     end
 
+    def load_config(debug = true)
+      @config = YAML.load_file(Moritan::CONF_FILE)
+
+      @function = Moritan::Function.new(@config['ReplayTable'], @config['Function'])
+      @name = debug ? @config['name_debug'] : @config['name']
+
+      oauth = debug ? 'oauth_debug' : 'oauth'
+      @CONSUMER_KEY       = @config[oauth]['consumer_key']
+      @CONSUMER_SECRET    = @config[oauth]['consumer_secret']
+      @OAUTH_TOEKN        = @config[oauth]['oauth_token']
+      @OAUTH_TOEKN_SECRET = @config[oauth]['oauth_token_secret']
+    end
 
     # 通常の投稿
-    def post(text = "", twitter_id:nil, status_id:nil, try:0)
-      # 会話の返事
-      if status_id
-        rep_text = "@#{twitter_id} #{text}"
-        if rep_text.size > 140
-          rep_text = self.chain_post(text,twitter_id:twitter_id,status_id:status_id)
-        end
-        self.client.update(rep_text,{:in_reply_to_status_id => status_id})
-        puts rep_text
+    def post(text = "", twitter_id:nil, status_id:nil)
+      text = twitter_id ? "@#{twitter_id} #{text}" : text
 
-      # ただの投稿(twitter_id:nil)か会話の始まり
-      else
-        post_text = twitter_id ? "@#{twitter_id} #{text}" : text
-        if post_text.size > 140
-          post_text = self.chain_post(text,twitter_id:twitter_id,status_id:status_id)
-        end
-        self.client.update(post_text)
-        puts post_text
+      if text.size > 140
+        over_text = text
+        twitter_id_size = twitter_id ? ("@#{twitter_id}".size + 1) : 0
+        # ↓”＠〜”と”（続く）”4文字を除いた最終的にpostに返せる最大文字数で分割
+        post_size = 140 - (twitter_id_size + 4)
+        post_text, *over_text = text.reverse.scan(/.{1,#{post_size}}/m)
+        post("#{over_text.join.reverse}(続く)", twitter_id:twitter_id, status_id:status_id)
       end
+
+      post_text = post_text ? post_text.reverse : text
+      self.client.update(post_text, in_reply_to_status_id: status_id)
+      puts post_text
 
     # Twitter::Error::RequestTimeout: exection expired
     rescue Twitter::Error
+      try ||= 0
       try += 1
       error_logs("#{try}回目のpost")
       sleep 1
       retry if try < 3
-    end
-
-
-    # 140文字を超える投稿(分割投稿後140文字以下の最後の投稿を返す)
-    def chain_post(text = "", twitter_id:nil, status_id:nil, try:0)
-      over_text = text
-      twitter_id_size = twitter_id ? ("@#{twitter_id}".size + 1) : 0
-
-      # ↓”＠〜”と”（続く）”4文字を除いた最終的にpostに返せる最大文字数で分割
-      post_size = 140 - (twitter_id_size + 4)
-      texts = over_text.scan(/.{1,#{post_size}}/m)
-
-      begin
-        0.upto(texts.size - 2) do |i|
-          texts[i] = twitter_id ? "@#{twitter_id} #{texts[i]}(続く)" : "#{texts[i]}(続く)"
-          self.client.update(texts[i],{:in_reply_to_status_id => status_id})
-          puts texts[i]
-        end
-      rescue Twitter::Error
-        try += 1
-        error_logs("#{try}回目のchain_post")
-        sleep 1
-        retry if try < 3
-      end
-
-      return "@#{twitter_id} #{texts[texts.size - 1]}"
     end
 
     # ふぁぼる
